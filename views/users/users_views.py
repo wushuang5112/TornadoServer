2fbcc338bbb2d585f31186e408ea57dd70de5c73
xsx <1349498580@qq.com>
2018-06-19 15:58:55 +0800

    log 级别及其历史保留记录

--- views/users/users_views.py
+++ views/users/users_views.py
@@ -3,6 +3,8 @@
 
 import tornado.web
 from tornado.escape import json_decode
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 # 从commons中导入http_response方法
 from common.commons import (
@@ -13,6 +15,20 @@ from common.commons import (
 from conf.base import (
     ERROR_CODE,
 )
+
+########## Configure logging #############
+logFilePath = "log/users/users.log"
+logger = logging.getLogger("Users")
+logger.setLevel(logging.DEBUG)
+handler = TimedRotatingFileHandler(logFilePath,
+                                   when="D",
+                                   interval=1,
+                                   backupCount=30)
+formatter = logging.Formatter('%(asctime)s \
+%(filename)s[line:%(lineno)d] %(levelname)s %(message)s',)
+handler.suffix = "%Y%m%d"
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class RegistHandle(tornado.web.RequestHandler):
@@ -30,9 +46,12 @@ class RegistHandle(tornado.web.RequestHandler):
             password = args['password']
             verify_code = args['code']
         except:
+            #获取入参失败时，抛出错误码及错误信息
+            logger.info("RegistHandle: request argument incorrect")
             # 获取入参失败时，抛出错误码及错误信息
             http_response(self, ERROR_CODE['1001'], 1001)
             return
 
-            # 处理成功后，返回成功码“0”及成功信息“ok”
+        # 处理成功后，返回成功码“0”及成功信息“ok”
+        logger.debug("RegistHandle: regist successfully")
         http_response(self, ERROR_CODE['0'], 0)
