9a2bd9be1f66e28139a29687003233331bfb3f14
xsx <1349498580@qq.com>
2018-06-23 00:56:23 +0800

    H5后台页面渲染

--- views/users/users_urls.py
+++ views/users/users_urls.py
@@ -4,10 +4,12 @@
 
 from __future__ import unicode_literals
 from .users_views import (
-    RegistHandle
+    RegistHandle,
+    LoginHandle
 )
 
 urls = [
     #从 /users/regist 过来的请求，将调用 users_views 里面的 RegistHandle 类
-    (r'regist', RegistHandle)
+    (r'regist', RegistHandle),
+    (r'login', LoginHandle)
 ]
--- views/users/users_views.py
+++ views/users/users_views.py
@@ -82,3 +82,40 @@ class RegistHandle(tornado.web.RequestHandler):
             # 处理成功后，返回成功码“0”及成功信息“ok”
             logger.debug("RegistHandle: regist successfully")
             http_response(self, ERROR_CODE['0'], 0)
+
+
+class LoginHandle(tornado.web.RequestHandler):
+    """
+    handle /user/login request
+    :param phone: users sign up phone
+    :param password: users sign up password
+    """
+
+    @property
+    def db(self):
+        return self.application.db
+
+    def get(self):
+        try:
+            # 获取入参
+            phone = self.get_argument("phone")
+            password = self.get_argument("password")
+        except:
+            # 获取入参失败时，抛出错误码及错误信息
+            logger.info("LoginHandle: request argument incorrect")
+            http_response(self, ERROR_CODE['1001'], 1001)
+            return
+
+            # 从数据库 Users 表查找入参中的 phone 是否存在
+        ex_user = self.db.query(Users).filter_by(phone=phone).first()
+        if ex_user:
+            # 如果手机号已存在，返回首页 H5 页面 index.html
+            logger.debug("LoginHandle: get user login: %s" % phone)
+            self.render("index.html")
+            self.db.close()
+            return
+        else:
+            # 用户不存在，提示用户未注册
+            http_response(self, ERROR_CODE['1003'], 1003 )
+            self.db.close()
+            return
